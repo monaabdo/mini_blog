4e8c40eaeeb0ef53a90e9b4b2bfda53afbfb8d33
monaabdo88 <monaabdo88@gmail.com>
2026-09-21 11:48:45 +0300

    add Insert & Update to the database class

--- vendor/System/Database.php
+++ vendor/System/Database.php
@@ -8,6 +8,11 @@ use PDOException;
 class Database
 {
     private static $connection;
+    private string $table;
+    private array $data;
+    private array $bindings =[];
+    private int $lastID;
+    private array $wheres = [];
     public function __construct(private Application $app)
     {
         if(! $this->isConnected())
@@ -25,6 +30,9 @@ class Database
         extract($connectionData);
         try{
             static::$connection = new PDO('mysql:'.$server.';dbname='.$dbname,$dbuser,$dbpass);
+            static::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+            static::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            static::$connection->exec('SET NAMES utf8');
         }catch(PDOException $e)
         {
             die($e->getMessage());
@@ -33,5 +41,110 @@ class Database
     public function connection()
     {
         return static::$connection;
+    }
+    public function setTable(string $table)
+    {
+        $this->table = $table;
+        return $this;
+    }
+    public function from(string $table)
+    {
+        return $this->setTable($table);
+    }
+    public function data(mixed $key , mixed $value)
+    {
+        if(is_array($key))
+        {
+            $this->data = array_merge($this->data , $key);
+            $this->addToBindings($key);
+        }
+        else{
+            $this->data[$key] = $value;
+            $this->addToBindings($key);
+        }
+        
+        return $this;
+    }
+    public function insert(string $table ='')
+    {
+        if($table) $this->table($table);
+           
+        $sql = "INSERT INTO $this->table SET";
+        $sql .= $this->setFields();
+        $this->query($sql , $this->bindings);
+        $this->lastID = $this->connection()->lastInsertId();
+        return $this;
+    }
+    public function update($table = '')
+    {
+        if($table) $this->table($table);
+           
+        $sql = "UPDATE $this->table SET";
+        $sql .= $this->setFields();
+
+        if($this->wheres)
+        {
+            $sql .=" WHERE ".implode(" ",$this->wheres);
+        }
+
+        $this->query($sql , $this->bindings);
+        return $this;
+    }
+    private function addToBindings(mixed $value)
+    {
+        if(is_array($value))
+        {
+            $this->bindings = array_merge($this->bindings , array_values($value));
+        }
+        else{
+            $this->bindings[] = _e($value);
+        }
+       
+    }
+    public function lastID():int
+    {
+        return $this->lastID;
+    }
+    public function query(...$bindings)
+    {
+        $sql = array_shift($bindings);
+        if(count($bindings) == 1 && is_array($bindings[0]))
+        {
+            $bindings = $bindings[0];
+        }
+        try{
+            $query = $this->connection()->prepare($sql);
+            foreach($bindings as $key => $value)
+            {
+                $query->bindValue($key+1, $value);
+            }
+            $query->execute();
+            return $query;
+        }catch(PDOException $e)
+        {
+            die($e->getMessage());
+        }
+    }
+    private function table(string $table)
+    {
+        $this->table = $table;
+    }
+    public function where(...$bindings)
+    {
+        $sql = array_shift($bindings);
+        $this->addToBindings($bindings);
+        $this->wheres[] = $sql;
+        return $this;
+    }
+    private function setFields():string
+    {
+        $sql = '';
+        foreach($this->data as $key => $value)
+        {
+            $sql .="`$key ` =? , ";
+            $this->addToBindings($value);
+        }
+        $sql = rtrim($sql , ", ");
+        return $sql;
     }
 }
